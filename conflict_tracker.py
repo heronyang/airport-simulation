be47e076d90bc0bccc154bfe278fe2baab2f205d
Heron Yang <heron.yang.tw@gmail.com>
2017-11-26 13:46:27 -0800

    Returns the conflicts found in prediction

--- conflict_tracker.py
+++ conflict_tracker.py
@@ -28,7 +28,6 @@ def add_conflict(conflict):
 def save_and_reset_conflicts():
     conflicts_snapshot = conflicts
     conflicts = []
-    return conflicts_snapshot
 
 def restore_conflicts():
     conflicts = conflicts_snapshot
