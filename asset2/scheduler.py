7973c649c10069e8208bb6f3015fcb2df894b313
Ritwik <ritwik.rajendra8119@gmail.com>
2017-11-25 22:04:34 -0800

    Added logging

--- asset2/scheduler.py
+++ asset2/scheduler.py
@@ -22,8 +22,8 @@ class Scheduler:
         # simulation.routing_expert : gets routes from node A to node B
         # time : current simulated time of a day
         self.logger.debug("Scheduling starts")
-        self.logger.debug("Found %d aircrafts",
-                          len(simulation.airport.aircrafts))
+        self.logger.debug("Found %d aircrafts %s",
+                          len(simulation.airport.aircrafts), simulation.airport.aircrafts)
 
         # helper function:
         # conflicts = simulation.predict_state_after(schedule, time_from_now)
@@ -39,17 +39,19 @@ class Scheduler:
             # Pulls outs the flight information
             flight = simulation.scenario.get_flight(aircraft)
 
-            if aircraft.is_idle and \
-               aircraft.location.is_close_to(flight.from_gate)  or aircraft.pilot.state == State.moving:
+            if aircraft.is_idle  or aircraft.pilot.state == State.moving:
 
                 # Gets the route from the routing expert
                 # NOTE: We use runway start node as the destination of a
                 # departure flight
 
+
+               if aircraft.location.is_close_to(flight.from_gate):
+
                	if aircraft.pilot.state == State.moving:
-               		from_location = aircraft.location
-               	else:
                		from_location = flight.from_gate 
+               	else:
+               		from_location = aircraft.location
                 
                 route = simulation.routing_expert.get_shortest_route(
                     from_location, flight.runway.start)
@@ -79,13 +81,14 @@ class Scheduler:
 	                    nodes_tightness[node] = time_diff if node not in nodes_tightness \
     	                	else min(time_diff, nodes_tightness[node])
 
-                    self.logger.debug("Route node is : %s, %s", node, get_seconds(arr_time))
+                    self.logger.debug("Target node is : %s, %s", node, arr_time)
                     target_nodes.append(
                         Itinerary.TargetNode(node, arr_time, dep_time))
                     
                     prev_node = node
 
                 itinerary = Itinerary(target_nodes, flight.departure_time)
+                self.logger.debug("Itinerary generated: %s for aircraft %s with departure_time %s", itinerary, aircraft, flight.departure_time)
                 requests.append(Schedule.Request(aircraft, itinerary))
                 self.logger.debug("Adds route %s on %s" % (route, aircraft))
 
@@ -99,6 +102,7 @@ class Scheduler:
         # Repeatedly check for conflicts and resolve it until conflict-free schedule is obtained
         count = 0
         while conflictInfo is not None: 
+            self.logger.debug("Conflict detected at : %s", conflictInfo)
             requests = self.resolve_conflict(conflictInfo, requests)
             conflictInfo = self.check_conflict(requests)
             count+=1
@@ -131,7 +135,7 @@ class Scheduler:
 
             for target_node in itinerary.target_nodes:
                 node = target_node.node
-                for time in range(get_seconds(target_node.expected_arrival_time), get_seconds(target_node.expected_departure_time)):
+                for time in range(get_seconds(target_node.expected_arrival_time)-60, get_seconds(target_node.expected_departure_time)+60):
                     if time not in occupany_map:
                         occupany_map[time] = {}
 
@@ -169,15 +173,17 @@ class Scheduler:
 
                 for idx2 in range(len(target_nodes)):
                     if target_nodes[idx2] == target_node1:
-                        delay = target_nodes[idx2+1].expected_arrival_time - target_node1.expected_departure_time
+                        delay = get_seconds(target_nodes[idx2+1].expected_arrival_time) - get_seconds(target_node1.expected_departure_time)
 
             if aircraft == aircraft2:
                 for target_node in itinerary.target_nodes:
                     target_node.expected_departure_time = get_seconds_after(target_node.expected_departure_time, delay)
-                    target_node.expected_arrival_time = get_seconds_after(delay, target_node.expected_arrival_time, delay)
+                    target_node.expected_arrival_time = get_seconds_after(target_node.expected_arrival_time, delay)
 
                 itinerary.start_time = get_seconds_after(itinerary.start_time, delay)
 
+                requests[idx].itinerary = itinerary
+            self.logger.debug("%s", requests[idx].itinerary)
         return requests
 
     def __getstate__(self):
