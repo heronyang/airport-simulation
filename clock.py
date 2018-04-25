844e6911229ea3fbe0f3b500562c433129af1097
Heron Yang <heron.yang.tw@gmail.com>
2018-04-25 12:32:59 -0700

    Adds early termination option in the config file

--- clock.py
+++ clock.py
@@ -10,12 +10,14 @@ class Clock:
         # Starts at 00:00
         self.now = time(0, 0)
         self.sim_time = Config.params["simulation"]["time_unit"]
+        end_time_raw = Config.params["simulation"]["end_time"].split(":")
+        self.end_time = time(int(end_time_raw[0]), int(end_time_raw[1]))
 
     def tick(self):
 
         time_after_tick = get_seconds_after(self.now, self.sim_time)
 
-        if time_after_tick < self.now:
+        if time_after_tick > self.end_time:
             raise ClockException("End of the day")
 
         self.now = time_after_tick
