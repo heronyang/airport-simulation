8c5b31d37fc1eb5456b96470de90da31bad74c3b
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-10-01 16:53:14 -0700

    Change shortest path algorithm from Floyd to SPFA.

--- routing_expert.py
+++ routing_expert.py
@@ -4,6 +4,7 @@ import cache
 
 from link import Link
 from route import Route
+from surface import Runway
 
 
 class RoutingExpert:
@@ -28,6 +29,7 @@ class RoutingExpert:
         # Saves all the links and nodes
         self.links = links
         self.nodes = nodes
+        self.runway_nodes = list(map(lambda l: l.start, list(filter(lambda l: type(l) == Runway, self.links))))
         self.logger.info("%d links and %d nodes are loaded",
                          len(self.links), len(self.nodes))
 
@@ -49,12 +51,15 @@ class RoutingExpert:
             # Builds the routes
             self.__build_routes()
             cache.put(hash_key, self.routing_table)
+            exit(0)  # TODO: debug
 
     def __build_routes(self):
+        import time  # TODO: debug
+        start = time.time()  # TODO: debug
 
         self.logger.debug("Starts building routes, # nodes: %d # links: %d",
                           len(self.nodes), len(self.links))
-        self.__init_routing_table()
+        self.__init_adjacent_map()
 
         # Step 1: Links two nodes with really short distance (using
         # is_close_to method)
@@ -65,38 +70,34 @@ class RoutingExpert:
         self.logger.debug("Starts linking existing links")
         self.__link_existing_links()
 
-        # Step 3: Applies Floyd–Warshall algorithm to get shortest routes for
-        # all node pairs
-        self.logger.debug("Starts floyd-warshall for finding shortest routes")
-        self.__finds_shortest_route()
+        # Step 3: Applies SPFA to get shortest routes for all node pairs
+        self.logger.debug("Starts SPFA for finding shortest routes")
+        self.__finds_shortest_route_spfa()
 
         # Prints result
-        self.print_route()
+        self.print_route()  # TODO: debug
 
-    def __init_routing_table(self):
+        end = time.time()  # TODO: debug
+        print("Time elapsed:", end - start)  # TODO: debug
 
-        # Initializes the routing table
-        for start in self.nodes:
-            self.routing_table[start] = {}
-            for end in self.nodes:
-                if start == end:
-                    self.routing_table[start][end] = None
-                else:
-                    self.routing_table[start][end] = Route(start, end, [])
+    def __init_adjacent_map(self):
+        # Initializes the adjacency map
+        # self.adjacency_map[src][dst] = link
+        self.adjacency_map = {}
+
+        for node in self.nodes:
+            self.adjacency_map[node] = {}
 
     def __link_close_nodes(self):
-
         counter = 0
 
         for start in self.nodes:
             for end in self.nodes:
                 if start != end and start.is_close_to(end):
                     link = Link("CLOSE_NODE_LINK", [start, end])
-                    self.routing_table[start][end].update_link(link)
+                    self.adjacency_map[start][end] = link
                     self.logger.debug("%s and %s are close node", start, end)
                     counter += 1
-                    if not self.routing_table[start][end].is_completed:
-                        raise Exception("Unable to link two close nodes")
 
         self.logger.debug("Adds %d links for close nodes", counter)
 
@@ -107,51 +108,56 @@ class RoutingExpert:
             end = link.end
 
             # If there's already a link exists, store the shortest one
-            route = self.routing_table[start][end]
-            route_rev = self.routing_table[end][start]
-            if route.distance > link.length:
-                route.update_link(link)
-                route_rev.update_link(link.reverse)
+            if end in self.adjacency_map[start] and self.adjacency_map[start][end].length < link.length:
+                continue
+            else:
+                self.adjacency_map[start][end] = link
+                self.adjacency_map[end][start] = link.reverse
 
-            if not (self.routing_table[start][end].is_completed and
-                    self.routing_table[end][start].is_completed):
-                raise Exception("Unable to link two ends of a link from %s"
-                                " to %s" % (start, end))
+    def __finds_shortest_route_spfa(self):
+        for r in self.runway_nodes:
+            self.routing_table[r] = {}
 
-    def __finds_shortest_route(self):
+            for n in self.nodes:
+                if n == r:
+                    continue
+                self.routing_table[r][n] = Route(n, r, [])
 
-        # Floyd-Warshall
-        for k in self.nodes:
-            for i in self.nodes:
-                for j in self.nodes:
+            queue = [r]
 
-                    r_ij = self.routing_table[i][j]
-                    r_ik = self.routing_table[i][k]
-                    r_kj = self.routing_table[k][j]
-
-                    # Ignores the nodes with no route in between
-                    if r_ik is None or r_kj is None or r_ij is None:
-                        continue
-
-                    # Ignores the cases where i -> k or k -> j is not connected
-                    if not r_ik.is_completed or not r_kj.is_completed:
-                        continue
-
-                    # Updates the i -> j route if i -> k -> j is shorter
-                    if r_ik.distance + r_kj.distance < r_ij.distance:
-                        r_ij.reset_links()
-                        r_ij.add_links(r_ik.links)
-                        r_ij.add_links(r_kj.links)
+            while queue:
+                u = queue.pop(0)
+                for v in self.adjacency_map[u]:
+                    new_distance = self.routing_table[r][u].distance + self.adjacency_map[u][v].length \
+                        if r != u else self.adjacency_map[u][v].length
+                    old_distance = self.routing_table[r][v].distance if r != v else 0
+                    if new_distance < old_distance:
+                        self.routing_table[r][v].reset_links()
+                        self.routing_table[r][v].add_link(self.adjacency_map[v][u])
+                        if r != u:
+                            self.routing_table[r][v].add_links(self.routing_table[r][u].links)
 
                         self.logger.debug("%s -> %s -> %s is shorter than "
-                                          "%s -> %s", i, k, j, i, j)
+                                          "%s -> %s", v, u, r, v, r)
+
+                        if v not in queue:
+                            queue.append(v)
+
+            for node in self.nodes:
+                # r is in the routing table; some nodes could be unreachable
+                if node not in self.routing_table[r] or not len(self.routing_table[r][node].links):
+                    continue
+                if not self.routing_table[r][node].is_completed:
+                    raise Exception("Incomplete route found.")
 
     def print_route(self):
         """Prints all the routes into STDOUT."""
 
-        for start in self.nodes:
+        for start in self.runway_nodes:
             for end in self.nodes:
-                self.logger.debug("[%s - %s]", start, end)
+                if start == end:
+                    continue
+                self.logger.debug("[%s - %s]", end, start)
                 route = self.routing_table[start][end]
                 if route:
                     self.logger.debug(route.description)
@@ -159,11 +165,15 @@ class RoutingExpert:
                     self.logger.debug("No Route")
 
     def get_shortest_route(self, start, end):
-        """Gets the shortest route by given start and end node."""
-        if (start not in self.routing_table) or \
-           (end not in self.routing_table[start]):
+        """
+        Gets the shortest route by given start and end node.
+        end node must be a runway node
+        """
+        if end not in self.runway_nodes:
+            raise Exception("End node is not a runway node.")
+        if start not in self.routing_table[end]:
             return None
-        return self.routing_table[start][end]
+        return self.routing_table[end][start]
 
     def __getstate__(self):
         attrs = dict(self.__dict__)
