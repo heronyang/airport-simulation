2e30fca0b0a5d76e1563451e049016cd554e680e
Zhongyi Tong <zhongyi.tong@foxmail.com>
2018-10-17 20:51:47 -0700

    Use custom map styling.

---
+++ visualization/static/js/views/main-view.js
@@ -0,0 +1,303 @@
+/* Map */
+let mapView;
+
+const FLIGT_ICON_URL = "/image/flight.png";
+const FLIGT_MOVING_ICON_URL = "/image/flight-moving.png";
+const FLIGT_HOLD_ICON_URL = "/image/flight-hold.png";
+const GATE_ICON_URL = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png";
+const SPOT_ICON_URL = "http://maps.google.com/mapfiles/ms/icons/green-dot.png";
+
+const QUICK_NEXT_PREV_TIMES = 120;
+
+function initMap() {
+    mapView = new MapView(document.getElementById("map"));
+}
+
+function getParams() {
+    var params = [], hash;
+    var hashes = window.location.href
+        .slice(window.location.href.indexOf("?") + 1)
+        .split("&");
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split("=");
+        params.push(hash[0]);
+        params[hash[0]] = hash[1];
+    }
+    return params;
+}
+
+/* UI Callbacks */
+$("#auto").click(function (e) {
+    console.log("auto");
+    e.preventDefault();
+    toggleAutoRun();
+});
+
+$("#prev").click(function (e) {
+    e.preventDefault();
+    prevState();
+});
+
+$("#next").click(function (e) {
+    e.preventDefault();
+    nextState();
+});
+
+$("#prev-prev").click(function (e) {
+    e.preventDefault();
+    for (var i = 0; i < QUICK_NEXT_PREV_TIMES; i++) {
+        prevState();
+    }
+});
+
+$("#next-next").click(function (e) {
+    e.preventDefault();
+    for (var i = 0; i < QUICK_NEXT_PREV_TIMES; i++) {
+        nextState();
+    }
+});
+
+/* UI Operations */
+function loadPlans(plans) {
+    var dropdown = $("#dropdown-plan");
+    dropdown.empty();
+    for (let plan of plans) {
+        dropdown.append('<a class="dropdown-item option-plan" href="#">' +
+            plan + '</a>');
+    }
+    $(".option-plan").click(function (e) {
+        e.preventDefault();
+        window.location.href = "?plan=" + e.target.text;
+    });
+}
+
+function setPlanShow(plan) {
+    $("#dropdown-plan-label").html(plan);
+}
+
+function getShownPlan() {
+    return $("#dropdown-plan-label").html();
+}
+
+function setCurrentTime(time) {
+    $("#current-time").text(time);
+}
+
+function setAutoRunShow(enabled) {
+    const button = $("#auto");
+    if (enabled) {
+        button.removeClass("btn-secondary").addClass("btn-primary");
+    } else {
+        button.removeClass("btn-primary").addClass("btn-secondary");
+    }
+}
+
+function showError(message) {
+    alert("Error: " + message);
+}
+
+/* API Adapter Functions */
+function getPlans(callback) {
+    $.get("/plans", function (data) {
+        callback(JSON.parse(data));
+    }).fail(function (jqXHR, textStatus) {
+        showError(jqXHR.responseText);
+    });
+}
+
+function getExprData(plan, callback) {
+    const url = "/expr_data?plan=" + plan;
+    $.get(url, function (data) {
+        callback(JSON.parse(data));
+    }).fail(function (jqXHR, textStatus) {
+        showError(jqXHR.responseText);
+    });
+}
+
+/* Controllers */
+var autoRunWorker = null;
+var pauseTime = 500;
+
+function toggleAutoRun() {
+
+    if (autoRunWorker) {
+        clearInterval(autoRunWorker);
+        setAutoRunShow(false);
+        autoRunWorker = null;
+    } else {
+        autoRunWorker = window.setInterval(function () {
+            nextState();
+        }, pauseTime);
+        setAutoRunShow(true);
+    }
+
+}
+
+/* Main */
+$("document").ready(function () {
+    loadOptions();
+    const params = getParams();
+    if ("plan" in params) {
+        const plan = params["plan"];
+        setPlanShow(plan);
+        loadExprData(plan);
+    }
+});
+
+function loadOptions() {
+    getPlans(function (plans) {
+        loadPlans(plans)
+    });
+}
+
+var expr_data = null;
+
+function loadExprData(plan) {
+    getExprData(plan, function (data) {
+        expr_data = data;
+        setAirportCenter();
+        drawSurfaceData();
+        updateState();
+    });
+}
+
+function setAirportCenter() {
+    var center = expr_data["surface"]["airport_center"];
+    mapView.init(center["lat"], center["lng"]);
+}
+
+function drawSurfaceData() {
+
+    // Gate
+    for (let gate of expr_data["surface"]["gates"]) {
+        const name = "GATE: " + gate["name"];
+        mapView.__drawNode(gate["lat"], gate["lng"], GATE_ICON_URL, "", name);
+    }
+
+    // Spot
+    for (let spot of expr_data["surface"]["spots"]) {
+        const name = "SPOT POSITION: " + spot["name"];
+        mapView.__drawNode(spot["lat"], spot["lng"], SPOT_ICON_URL, "", name);
+    }
+
+    // Runway
+    for (let runway of expr_data["surface"]["runways"]) {
+        mapView.drawRunway(parseNodes(runway["nodes"]));
+    }
+
+    // Pushback way
+    for (let pushback_way of expr_data["surface"]["pushback_ways"]) {
+        mapView.drawPushbackWay(parseNodes(pushback_way["nodes"]));
+    }
+
+    // Taxiway
+    for (let taxiway of expr_data["surface"]["taxiways"]) {
+        mapView.drawTaxiway(parseNodes(taxiway["nodes"]));
+    }
+}
+
+function parseNodes(rawNodes) {
+    var nodes = [];
+    for (let node of rawNodes) {
+        nodes.push({"lat": node[1], "lng": node[0]});
+    }
+    return nodes;
+}
+
+/* State */
+var state_index = 0;
+
+function nextState() {
+    state_index = (state_index + 1) % expr_data["state"].length;
+    updateState();
+}
+
+function prevState() {
+    state_index -= 1;
+    if (state_index < 0) {
+        state_index += expr_data["state"].length;
+    }
+    updateState();
+}
+
+var aircrafts = [];
+
+function updateState() {
+
+    // Clean previous state
+    for (var i = 0; i < aircrafts.length; i++) {
+        aircrafts[i].setMap(null);
+    }
+    aircrafts = [];
+
+    // Current time
+    const state = expr_data["state"][state_index];
+    setCurrentTime(state["time"]);
+
+    // Aircrafts
+    for (let aircraft of expr_data["state"][state_index]["aircrafts"]) {
+        aircrafts.push(mapView.__drawNode(
+            aircraft["location"]["lat"], aircraft["location"]["lng"],
+            getAircraftIconUrl(aircraft),
+            aircraft["callsign"],
+            parseAircraftContent(aircraft),
+            true
+        ));
+    }
+
+}
+
+function parseAircraftContent(aircraft) {
+    var html = aircraft["callsign"] + "</br>";
+    if (aircraft["itinerary"] == null) {
+        return html + "No itinerary";
+    }
+    html += "<table><tr><th>Target</th><th>LatLng</th><th>UC</th><th>SC</th></tr>";
+    var index = aircraft["itinerary_index"];
+    var uc_delay_index = aircraft["uncertainty_delayed_index"];
+    var sc_delay_index = aircraft["scheduler_delayed_index"];
+    var i = 0;
+    for (let target of aircraft["itinerary"]) {
+        var latlng = target["node_location"]["lat"] + "," +
+            target["node_location"]["lng"];
+        html += "<tr class=\"" + getTargetClassName(i, index) + "\">";
+        html += "<td>" + target["node_name"] + "</td><td>" + latlng + "</td>";
+        if ($.inArray(i, uc_delay_index) >= 0) {
+            html += "<td>V</td>";
+        } else {
+            html += "<td>&nbsp;</td>";
+        }
+        if ($.inArray(i, sc_delay_index) >= 0) {
+            html += "<td>V</td>";
+        } else {
+            html += "<td>&nbsp;</td>";
+        }
+        html += "</tr>";
+        i += 1;
+    }
+    html += "</table>";
+    return html;
+}
+
+function getTargetClassName(current_index, index) {
+    if (current_index < index) {
+        return "past-target";
+    } else if (current_index == index) {
+        return "current-target";
+    }
+    return "future-target";
+}
+
+function getAircraftIconUrl(aircraft) {
+
+    if (aircraft["state"] == "stop") {
+        return FLIGT_ICON_URL;
+    }
+
+    if (aircraft["is_delayed"]) {
+        return FLIGT_HOLD_ICON_URL;
+    }
+
+    return FLIGT_MOVING_ICON_URL;
+
+}
--- visualization/static/js/views/map-view.js
+++ visualization/static/js/views/map-view.js
@@ -1,50 +1,143 @@
 "use strict";
 
 // NASA Ame Research Center
-const DEFAULT_LAT = 37.4088, DEFAULT_LNG = -122.0644, DEFAULT_ZOOM = 17;
+const DEFAULT_LAT = 37.4088, DEFAULT_LNG = -122.0644, DEFAULT_ZOOM = 16;
+const MAP_STYLE = [
+    {
+        "elementType": "labels",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "administrative.neighborhood",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#ffcc80"
+        }, {
+            "weight": 1
+        }]
+    }, {
+
+        "featureType": "road",
+        "stylers": [{
+            "visibility": "off"
+        }]
+    }, {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "lightness": 10
+        }]
+    }, {
+        "featureType": "transit.station.airport",
+        "elementType": "geometry.stroke",
+        "stylers": [{
+            "color": "#e8e5ea"
+        }, {
+            "lightness": -15
+        }]
+    }, {
+        "featureType": "water",
+        "stylers": [{
+            "saturation": 25
+        }]
+    }, {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "color": "#dfeaef"
+        }]
+    }
+];
+
+const ZOOM_FACTORS = {
+    20: 1128.497220,
+    19: 2256.994440,
+    18: 4513.988880,
+    17: 9027.977761,
+    16: 18055.955520,
+    15: 36111.911040,
+    14: 72223.822090,
+    13: 144447.644200,
+    12: 288895.288400,
+    11: 577790.576700,
+    10: 1155581.153000,
+    9: 2311162.307000,
+    8: 4622324.614000,
+    7: 9244649.227000,
+    6: 18489298.450000,
+    5: 36978596.910000,
+    4: 73957193.820000,
+    3: 147914387.600000,
+    2: 295828775.300000,
+    1: 591657550.500000
+};
+
+const RUNWAY_UNIT_LINK_WEIGHT = 12 * ZOOM_FACTORS[DEFAULT_ZOOM],
+    TAXIWAY_UNIT_LINK_WEIGHT = 12 * ZOOM_FACTORS[DEFAULT_ZOOM],
+    PUSHBACKWAY_UNIT_LINK_WEIGHT = 8 * ZOOM_FACTORS[DEFAULT_ZOOM];
 
 class MapView {
     constructor(rootElement) {
         this.map = new google.maps.Map(rootElement, {
             center: {lat: DEFAULT_LAT, lng: DEFAULT_LNG},
             zoom: DEFAULT_ZOOM,
-            styles: [
-                {
-                    featureType: "all",
-                    elementType: "labels",
-                    stylers: [{visibility: "off"}]
-                },
-            ]
+            mapTypeControlOptions: {
+                style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+                position: google.maps.ControlPosition.TOP_RIGHT
+            },
+            zoomControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            rotateControl: false,
+            fullscreenControl: false,
+            styles: MAP_STYLE
         });
 
+        this.runways = [];
+        this.pushbackways = [];
+        this.taxiways = [];
+
+        this.map.addListener('zoom_changed', this.__zoomChangeHandler.bind(this));
     }
 
     init(lat = DEFAULT_LAT, lng = DEFAULT_LAT, zoom = DEFAULT_ZOOM) {
         this.map.setOptions({
             center: new google.maps.LatLng(lat, lng),
-            zoom: zoom,
-            styles: [
-                {
-                    featureType: "all",
-                    elementType: "labels",
-                    stylers: [{visibility: "off"}]
-                },
-            ]
+            zoom: zoom
         });
     }
 
-    __drawLink(nodes, color) {
+    __drawLink(nodes, color, weight) {
         const link = new google.maps.Polyline({
             path: nodes,
             strokeColor: color,
+            strokeOpacity: 0.8,
+            strokeWeight: weight
         });
         link.setMap(this.map);
+        return link;
     }
 
-    drawRunway() {
-
+    drawRunway(nodes) {
+        const link = this.__drawLink(nodes, "#ffcc80", RUNWAY_UNIT_LINK_WEIGHT / ZOOM_FACTORS[this.map.getZoom()]);
+        this.runways.push(link);
     }
 
+    drawTaxiway(nodes) {
+        const link = this.__drawLink(nodes, "#4db6ac", TAXIWAY_UNIT_LINK_WEIGHT / ZOOM_FACTORS[this.map.getZoom()]);
+        this.taxiways.push(link);
+    }
+
+    drawPushbackWay(nodes) {
+        const link = this.__drawLink(nodes, "#90a4ae", PUSHBACKWAY_UNIT_LINK_WEIGHT / ZOOM_FACTORS[this.map.getZoom()]);
+        this.pushbackways.push(link);
+    }
 
     __drawNode(lat, lng, icon_url, label, content, is_center) {
         const infowindow = new google.maps.InfoWindow({
@@ -78,5 +171,35 @@ class MapView {
         });
 
         return marker;
+    }
+
+    drawAircraft() {
+
+    }
+
+    drawGate() {
+
+    }
+
+    drawSpot() {
+
+    }
+
+    __zoomChangeHandler() {
+        for (let runway of this.runways) {
+            runway.setOptions({
+                strokeWeight: RUNWAY_UNIT_LINK_WEIGHT / ZOOM_FACTORS[this.map.getZoom()]
+            });
+        }
+        for (let taxiway of this.taxiways) {
+            taxiway.setOptions({
+                strokeWeight: TAXIWAY_UNIT_LINK_WEIGHT / ZOOM_FACTORS[this.map.getZoom()]
+            });
+        }
+        for (let pushbackway of this.pushbackways) {
+            pushbackway.setOptions({
+                strokeWeight: PUSHBACKWAY_UNIT_LINK_WEIGHT / ZOOM_FACTORS[this.map.getZoom()]
+            });
+        }
     }
 }
